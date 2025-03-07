d0db120e3b7d5b852e47221b0a878156d9ee06de
Codesessor2000 <chaitanya.malegaonkar@gmail.com>
2025-03-07 12:45:06 +0530

    Initial project setup

---
+++ backend/social-media-feed-backend/src/main/java/com/socialmedia/social_media_feed_backend/SocialMediaFeedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.social_media_feed_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaFeedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaFeedBackendApplication.class, args);
+	}
+
+}
